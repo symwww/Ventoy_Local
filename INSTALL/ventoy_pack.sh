bbb67fd8aebea057f40d6e42bf2cfe6963b6390e
孙一鸣 <241880310@smail.nju.edu.cn>
2025-02-15 15:21:56 +0800

    Fix pack scripts

--- INSTALL/ventoy_pack.sh
+++ INSTALL/ventoy_pack.sh
@@ -31,22 +31,7 @@ cd ../Unix
 sh pack_unix.sh
 cd -
 
-cd ../LinuxGUI
-sh language.sh || exit 1
-sh build.sh
-cd -
-
-cd ../Plugson
-sh build.sh
-sh pack.sh
-cd -
-
-cd ../Vlnk
-sh build.sh
-sh pack.sh
-cd -
-
-
+mknod -m 666  /dev/loop0 b 7 0
 LOOP=$(losetup -f)
 
 rm -f img.bin
@@ -66,7 +51,6 @@ $GRUB_DIR/sbin/grub-bios-setup  --skip-fs-probe  --directory="./grub/i386-pc"  $
 curver=$(get_ventoy_version_from_cfg ./grub/grub.cfg)
 
 tmpmnt=./ventoy-${curver}-mnt
-tmpdir=./ventoy-${curver}
 
 rm -rf $tmpmnt
 mkdir -p $tmpmnt
@@ -129,154 +113,16 @@ dd status=none bs=1024 count=16  if=./tool/aarch64/vtoycli of=$tmpmnt/tool/mount
 
 
 # rm -f $tmpmnt/grub/i386-pc/*.img
-
-
+mkdir -p $tmpmnt/install
+dd if=$LOOP of=$tmpmnt/install/boot.img bs=1 count=512  status=none
+dd if=$LOOP of=$tmpmnt/install/core.img bs=512 count=2047 skip=1 status=none
+cd $tmpmnt/../
+tar -czvf ventoy-${curver}.tar.gz $tmpmnt
 umount $tmpmnt && rm -rf $tmpmnt
-
-
-rm -rf $tmpdir
-mkdir -p $tmpdir/boot
-mkdir -p $tmpdir/ventoy
-echo $curver > $tmpdir/ventoy/version
-dd if=$LOOP of=$tmpdir/boot/boot.img bs=1 count=512  status=none
-dd if=$LOOP of=$tmpdir/boot/core.img bs=512 count=2047 skip=1 status=none
-xz --check=crc32 $tmpdir/boot/core.img
-
-cp $OPT ./tool $tmpdir/
-rm -f $tmpdir/ENROLL_THIS_KEY_IN_MOKMANAGER.cer
-cp $OPT Ventoy2Disk.sh $tmpdir/
-cp $OPT VentoyWeb.sh $tmpdir/
-cp $OPT VentoyPlugson.sh $tmpdir/
-cp $OPT VentoyVlnk.sh $tmpdir/
-cp $OPT VentoyGUI* $tmpdir/
-
-
-cp $OPT README $tmpdir/
-cp $OPT plugin $tmpdir/
-cp $OPT CreatePersistentImg.sh $tmpdir/
-cp $OPT ExtendPersistentImg.sh $tmpdir/
-dos2unix -q $tmpdir/Ventoy2Disk.sh
-dos2unix -q $tmpdir/VentoyWeb.sh
-dos2unix -q $tmpdir/VentoyPlugson.sh
-dos2unix -q $tmpdir/VentoyVlnk.sh
-
-
-dos2unix -q $tmpdir/CreatePersistentImg.sh
-dos2unix -q $tmpdir/ExtendPersistentImg.sh
-
-cp $OPT ../LinuxGUI/WebUI $tmpdir/
-sed 's/.*SCRIPT_DEL_THIS \(.*\)/\1/g' -i $tmpdir/WebUI/index.html
-
-#32MB disk img
-dd status=none if=$LOOP of=$tmpdir/ventoy/ventoy.disk.img bs=512 count=$VENTOY_SECTOR_NUM skip=$part2_start_sector
-
-
-#4k image
-# echo "make 4K img ..."
-# dd status=none if=/dev/zero of=$tmpdir/ventoy/ventoy_4k.disk.img bs=1M count=32
-# mkfs.vfat -F 16 -n VTOYEFI -s 1 -S 4096 $tmpdir/ventoy/ventoy_4k.disk.img
-# vDIR1=$(mktemp -d)
-# vDIR2=$(mktemp -d)
-# mount $tmpdir/ventoy/ventoy.disk.img $vDIR1
-# mount $tmpdir/ventoy/ventoy_4k.disk.img $vDIR2
-# cp -a $vDIR1/* $vDIR2/
-# umount $vDIR1
-# umount $vDIR2
-# rm -rf $vDIR1 $vDIR2
-
-# xz --check=crc32 $tmpdir/ventoy/ventoy_4k.disk.img
-
-xz --check=crc32 $tmpdir/ventoy/ventoy.disk.img
-
-
-
 losetup -d $LOOP && rm -f img.bin
 
-rm -f ventoy-${curver}-linux.tar.gz
 
-
-CurDir=$PWD
-
-for d in i386 x86_64 aarch64 mips64el; do
-    cd $tmpdir/tool/$d
-    for file in $(ls); do
-        if [ "$file" != "xzcat" ]; then
-            if echo "$file" | grep -q '^Ventoy2Disk'; then
-                chmod +x $file
-            else
-                xz --check=crc32 $file
-            fi
-        fi
-    done
-    cd $CurDir
-done
-
-#chmod 
-find $tmpdir/ -type d -exec chmod 755 "{}" +
-find $tmpdir/ -type f -exec chmod 644 "{}" +
-chmod +x $tmpdir/Ventoy2Disk.sh
-chmod +x $tmpdir/VentoyWeb.sh
-chmod +x $tmpdir/VentoyPlugson.sh
-chmod +x $tmpdir/VentoyVlnk.sh
-chmod +x $tmpdir/VentoyGUI*
-chmod +x $tmpdir/tool/*.sh
-
-for d in i386 x86_64 aarch64 mips64el; do
-    chmod +x $tmpdir/tool/$d/xzcat
-    chmod +x $tmpdir/tool/$d/Ventoy2Disk.*
-done
-
-
-cp $OPT $LANG_DIR/languages.json $tmpdir/tool/
-
-
-chmod +x $tmpdir/CreatePersistentImg.sh
-chmod +x $tmpdir/ExtendPersistentImg.sh
-
-tar -czvf ventoy-${curver}-linux.tar.gz $tmpdir
-
-
-
-rm -f ventoy-${curver}-windows.zip
-
-cp $OPT Ventoy2Disk.exe $tmpdir/
-cp $OPT VentoyPlugson.exe $tmpdir/
-cp $OPT VentoyVlnk.exe $tmpdir/
-cp $OPT FOR_X64_ARM.txt $tmpdir/
-mkdir -p $tmpdir/altexe
-cp $OPT Ventoy2Disk_*.exe $tmpdir/altexe/
-cp $OPT VentoyPlugson_*.exe $tmpdir/altexe/
-
-
-
-cp $OPT $tmpdir/tool/plugson.tar.xz $tmpdir/ventoy/
-cp $OPT $LANG_DIR/languages.json $tmpdir/ventoy/
-rm -rf $tmpdir/tool
-rm -f $tmpdir/*.sh
-rm -f $tmpdir/VentoyGUI.*
-rm -rf $tmpdir/WebUI
-rm -f $tmpdir/README
-
-
-zip -r ventoy-${curver}-windows.zip $tmpdir/
-
-rm -rf $tmpdir
-
-echo "=============== run livecd.sh ==============="
-cd ../LiveCDGUI
-sh livecd.sh $1
-cd $CurDir
-
-mv ../LiveCDGUI/ventoy*.iso ./
-
-if [ -e ventoy-${curver}-windows.zip ] && [ -e ventoy-${curver}-linux.tar.gz ]; then
-    echo -e "\n ============= SUCCESS =================\n"
-else
-    echo -e "\n ============= FAILED =================\n"
-    exit 1
-fi
 
 rm -f log.txt
 rm -f sha256.txt
-sha256sum ventoy-${curver}-* > sha256.txt
 
